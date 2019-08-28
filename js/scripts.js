7bc8a47d2543433d80c65ecfc26e36f778877651
ian1017 <mdawidamengo@gmail.com>
2019-08-28 16:43:41 +0300

    add validation

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+function validate() {
+  var genders = document.getElementsByName("gender");
+  if( document.myForm.century.value == "" || isNaN( document.myForm.century.value ) ||
+  document.myForm.century.value.length > 2) {
+     alert( "Please provide a valid Century of birth! eg 20 for the year 2019" );
+     document.myForm.century.focus() ;
+     return false;
+  }
